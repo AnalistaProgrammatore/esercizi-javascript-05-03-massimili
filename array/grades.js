f1953226c7883831a35dc8223072719b95b92548
Massimiliano Puca <max.puca@gmail.com>
2020-03-14 20:22:38 +0100

    0314

--- array/grades.js
+++ array/grades.js
@@ -1,1 +1,27 @@
-
+function studente() {
+    this.voti = []
+    this.aggiungi = aggiungi
+    this.media = media
+}
+function aggiungi(voto) {
+    this.voti.push(voto)
+}
+function media() {
+    var totale = 0
+    for (var i = 0; i < this.voti.length; ++i) {
+        totale += this.voti[i]
+    }
+    return totale / this.voti.length
+}
+var mario = new studente()
+mario.aggiungi(30)
+mario.aggiungi(25)
+mario.aggiungi(25)
+mario.aggiungi(21)
+mario.aggiungi(25)
+mario.aggiungi(25)
+mario.aggiungi(20)
+mario.aggiungi(25)
+mario.aggiungi(19)
+mario.aggiungi(18)
+console.log(mario.media())
--- array/words.js
+++ array/words.js
@@ -1,1 +1,29 @@
+var parole = []
+function aggiungiParole(parola) {
+    parole.push(parola)
+}
+function leggi() {
+    var frase = ""
+    for (i = 0; i < parole.length; i++) {
+        frase = frase + parole[i] + " "
+    }
+    console.log(frase)
+}
+function leggiAlContrario() {
+    var frase = ""
+    for (i = 0; i < parole.length; i++) {
+        frase = parole[i] + " " + frase
+    }
+    console.log(frase)
+}
+
+aggiungiParole("Nel")
+aggiungiParole("mezzo")
+aggiungiParole("del")
+aggiungiParole("cammin")
+aggiungiParole("di")
+aggiungiParole("nostra")
+aggiungiParole("vita")
+leggi(parole)
+leggiAlContrario(parole)
 
