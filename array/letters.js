97d9818209fc24f426367dca7ff2ffa2944361a8
Massimiliano Puca <max.puca@gmail.com>
2020-03-15 10:43:09 +0100

    03151

--- array/letters.js
+++ array/letters.js
@@ -1,1 +1,29 @@
+function parola() {
+    this.lettere = []
+    this.aggiungi = aggiungi
+    this.concatena = concatena
+}
+function aggiungi(lettera) {
+    this.lettere.push(lettera)
+}
+function concatena() {
+    var risultato = ""
+    for (var i = 0; i < this.lettere.length; ++i) {
+        risultato = risultato + this.lettere[i]
+    }
+    return risultato
+}
+var leggi = new parola()
+leggi.aggiungi("B")
+leggi.aggiungi("U")
+leggi.aggiungi("O")
+leggi.aggiungi("N")
+leggi.aggiungi("G")
+leggi.aggiungi("I")
+leggi.aggiungi("O")
+leggi.aggiungi("R")
+leggi.aggiungi("N")
+leggi.aggiungi("O")
+leggi.aggiungi("!")
+console.log(leggi.concatena())
 
--- array/weekly.js
+++ array/weekly.js
@@ -1,1 +1,64 @@
+function weekTemps() {
+    this.dataStore = [[], [], [], []];
+    this.add = add;
+    this.mediaMensile = mediaMensile;
+    this.mediaSettimanale = mediaSettimanale;
+}
+function add(settimana, temp) {
+    this.dataStore[settimana - 1].push(temp);
+}
+function mediaMensile() {
+    var total = 0;
+    var elementi = 0
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        for (var j = 0; j < this.dataStore[i].length; ++j) {
+            total += this.dataStore[i][j];
+            elementi += 1
+        }
+    }
+    return total / elementi;
+}
+function mediaSettimanale(settimana) {
+    var total = 0;
+    for (var j = 0; j < this.dataStore[settimana - 1].length; ++j) {
+        total += this.dataStore[settimana - 1][j];
+    }
 
+    return total / this.dataStore[settimana - 1].length;
+}
+var thisWeek = new weekTemps();
+
+thisWeek.add(1, 52);
+thisWeek.add(1, 55);
+thisWeek.add(1, 61);
+thisWeek.add(1, 65);
+thisWeek.add(1, 55);
+thisWeek.add(1, 50);
+thisWeek.add(1, 52);
+thisWeek.add(2, 49);
+thisWeek.add(2, 52);
+thisWeek.add(2, 55);
+thisWeek.add(2, 61);
+thisWeek.add(2, 65);
+thisWeek.add(2, 55);
+thisWeek.add(2, 50);
+thisWeek.add(3, 52);
+thisWeek.add(3, 49);
+thisWeek.add(3, 52);
+thisWeek.add(3, 55);
+thisWeek.add(3, 61);
+thisWeek.add(3, 65);
+thisWeek.add(3, 55);
+thisWeek.add(4, 50);
+thisWeek.add(4, 52);
+thisWeek.add(4, 49);
+thisWeek.add(4, 52);
+thisWeek.add(4, 55);
+thisWeek.add(4, 61);
+thisWeek.add(4, 65);
+console.log("Media mensile " + thisWeek.mediaMensile());
+var sett = 2
+console.log("Media settimana " + sett + " " + thisWeek.mediaSettimanale(sett))
+for (sett = 1; sett < 5; ++sett) {
+    console.log("Media settimana " + sett + " " + thisWeek.mediaSettimanale(sett))
+}
