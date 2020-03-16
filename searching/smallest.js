a38523e96219a2455057ebc08fef1a3c2197c1b5
Massimiliano Puca <max.puca@gmail.com>
2020-03-16 15:52:43 +0100

    completato searching

--- searching/smallest.js
+++ searching/smallest.js
@@ -1,1 +1,8 @@
-
+var nums = [];
+for (var i = 0; i < 1000; ++i) {
+    nums[i] = Math.floor(Math.random() * 1001);
+}
+nums.sort((a, b) => a - b)
+console.log(nums)
+var smallest = 2
+console.log("Nella graduatoria dei più piccoli al posto " + smallest + " si trova il numero " + nums[smallest - 1])
