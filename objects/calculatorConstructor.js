3606685aa3a58a21f8d9ed238ac9a3bd50579952
Massimiliano Puca <max.puca@gmail.com>
2020-03-18 18:15:53 +0100

    completato

--- objects/calculatorConstructor.js
+++ objects/calculatorConstructor.js
@@ -1,1 +1,30 @@
+let calculator = function (values) {
+    this.values = values,
+        this.sum = function () {
+            let risultato = this.values.reduce((x, y) => x + y)
+            return risultato
+        },
+        this.sub = function () {
+            let risultato = this.values.reduce((x, y) => x - y)
+            return risultato
+        },
+        this.mul = function () {
+            let risultato = this.values.reduce((x, y) => x * y)
+            return risultato
+        },
+        this.div = function () {
+            let risultato = this.values.reduce((x, y) => x / y)
+            if (risultato == Infinity) {
+                return "infinito"
+            }
+            else {
+                return risultato
+            }
+        }
+}
 
+calculator = new calculator([10, 5]);
+console.log(calculator.sum())
+console.log(calculator.sub())
+console.log(calculator.mul())
+console.log(calculator.div())
