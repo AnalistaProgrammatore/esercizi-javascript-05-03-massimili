936e4b27a45e8d49e7ec1d490cc4d8574c46ca67
Massimiliano Puca <max.puca@gmail.com>
2020-03-18 17:36:25 +0100

    chaining

--- objects/chaining.js
+++ objects/chaining.js
@@ -1,1 +1,20 @@
+var oggetto = function () {
+    this.step = 0
 
+    this.up = function () {
+        this.step += 1
+        return this
+    }
+
+    this.down = function () {
+        this.step -= 1
+        return this
+    }
+
+    this.showStep = function () {
+        console.log(this.step)
+    }
+}
+var ladder = new oggetto()
+
+ladder.up().up().down().showStep() 
