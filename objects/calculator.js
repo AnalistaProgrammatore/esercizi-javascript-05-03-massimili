7a5c7c63dd4919c03469b87b5c3243cc474ba581
Massimiliano Puca <max.puca@gmail.com>
2020-03-18 11:34:36 +0100

    calculator OK

--- objects/calculator.js
+++ objects/calculator.js
@@ -1,1 +1,30 @@
+let calculator = {
+    values: [],
+    sum: function () {
+        let risultato = this.values.reduce((x, y) => x + y)
+        return risultato
+    },
+    sub: function () {
+        let risultato = this.values.reduce((x, y) => x - y)
+        return risultato
+    },
+    mul: function () {
+        let risultato = this.values.reduce((x, y) => x * y)
+        return risultato
+    },
+    div: function () {
+        let risultato = this.values.reduce((x, y) => x / y)
+        if (risultato == Infinity) {
+            return "infinito"
+        }
+        else {
+            return risultato
+        }
+    }
+}
 
+calculator.values = [10, 5];
+console.log(calculator.sum())
+console.log(calculator.sub())
+console.log(calculator.mul())
+console.log(calculator.div())
